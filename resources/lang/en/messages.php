ca16de132b3941a9cbac150f36c958b815e2b1e5
Ronald <ronald.jian@gmail.com>
2018-07-08 03:50:21 +0800

    modify lang file for en

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'login'   => 'Login',
+    'logout'   => 'Logout',
+    'register' => 'Register',
+    'home' => 'Home',
+    'dashboard' => 'Dashboard',
+    'you are logged in' => 'You are logged in.',
+    'name' => 'Name',
+    'password' => 'Password',
+    'confirm password' => 'Confirm password',
+    'remember me' => 'Remember me',
+    'forgot your password' => 'Forgot your password ?',
+    'currency' => 'Currency',
+    'published_at' => 'Published at',
+
+    'New Taiwan Dollar' => 'New Taiwan Dollar',
+    'US Dollar' => 'US Dollar',
+    'Japan, Yen' => 'Japan, Yen',
+    'China Yuan Renminbi' => 'China Yuan Renminbi',
+
+    'current locale' => 'Current Locale',
+    'current currency' => 'Current currency',
+    'current timezone' => 'Current timezone',
+    'current ip from' => 'Current IP 來自',
+    'current accept-language' => 'Accept-Language',
+
+];
